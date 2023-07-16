711538bec8ade74dd73f4ff7b69d2fa8723e6cee
D Anuj Kumar <anuj.as828@gmail.com>
2023-07-17 00:20:18 +0530

    deleted .env

--- db/db.js
+++ db/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const db = async () => {
   try {
     mongoose.set("strictQuery", false);
-    await mongoose.connect(process.env.MONGO_URL);
+    await mongoose.connect("mongodb+srv://p4sngh:8VTD25VxFSIJxRFl@cluster0.z0rc1vg.mongodb.net/?retryWrites=true&w=majority");
     console.log("Database Connected!");
   } catch (error) {
     console.log("DB Connection Error");
